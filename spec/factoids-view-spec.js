76f64217fb36b23c2b503e3cb5592a592d5f1130
mashabow <mashabow@shiromoji.net>
2015-12-12 22:21:35 +0900

    Generate JavaScript package

---
+++ spec/factoids-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FactoidsView from '../lib/factoids-view';
+
+describe('FactoidsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
