7a2e7d471c9db51f55314c154a7034c119e00bb0
mashabow <mashabow@shiromoji.net>
2015-12-13 17:36:06 +0900

    Remove view-related code

--- spec/factoids-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import FactoidsView from '../lib/factoids-view';
-
-describe('FactoidsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
